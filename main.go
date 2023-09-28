e9b1e1f883a1362098763633d4532d931b17734e
goobric <goobric@gmail.com>
2023-09-28 18:09:08 +0800

    link to url

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const url = "https://lco.dev"
+
+func main() {
+	fmt.Println("LCO web request")
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Response is of type %T\n", response)
+
+	response.Body.Close() // callers responsibility to close the connection
+}
