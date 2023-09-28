b973bead762572af14e7b3e2ac778d39506720e9
goobric <goobric@gmail.com>
2023-09-28 18:43:04 +0800

    retrieving info from url

--- urls.go
+++ urls.go
@@ -18,5 +18,23 @@ func urls(){
 	fmt.Println(result.Host)
 	fmt.Println(result.Path)
 	fmt.Println(result.Port()) //Port() is a method
-	fmt.Println(result.RawQuery)	
+	fmt.Println(result.RawQuery)
+	// query parameters
+	qparams := result.Query()
+	fmt.Printf("The data type of query-params are: %T\n", qparams)
+
+	fmt.Println(qparams["coursename"])
+
+	for _, val := range qparams{
+		fmt.Println("QParam is: ", val)
+	}
+
+	partsOfUrl := &url.URL{
+		Scheme: "https",
+		Host: "lco.dev",
+		Path: "/tutcss",
+		RawQuery: "user=hitesh",
+	}
+	completeURL := partsOfUrl.String()
+	fmt.Println(completeURL)
 }
